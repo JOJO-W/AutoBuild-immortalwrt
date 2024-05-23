2316ed9b911669afee5546a913738efe856d337f
WenJun <nczywq@qq.com>
2024-05-23 09:54:27 +0800

    sublime

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 
 # 移除要替换的包
-# rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -25,6 +25,8 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
 # 添加主题
 git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/luci-theme-design
 git clone https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
